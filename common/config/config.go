422dc640ba88b4bca460ef9cf62a16d689dedec0
liupan@onchain.com <liupan@onchain.com>
2018-05-28 10:43:20 +0800

    Fix serialize issue and add dht configure parameter
    Signed-off-by: liupan@onchain.com <liupan@onchain.com>

--- common/config/config.go
+++ common/config/config.go
@@ -37,35 +37,44 @@ const (
 
 var Version string
 
+type DHTNode struct {
+	PubKey  string `json:"PubKey"`
+	IP      string `json:"IP"`
+	UDPPort uint16 `json:"UDPPort"`
+	TCPPort uint16 `json:"TCPPort"`
+}
+
 type Configuration struct {
-	Magic             int64            `json:"Magic"`
-	Version           int              `json:"Version"`
-	SeedList          []string         `json:"SeedList"`
-	Bookkeepers       []string         `json:"Bookkeepers"` // The default book keepers' publickey
-	HttpRestPort      int              `json:"HttpRestPort"`
-	HttpCertPath      string           `json:"HttpCertPath"`
-	HttpKeyPath       string           `json:"HttpKeyPath"`
-	HttpInfoPort      uint16           `json:"HttpInfoPort"`
-	HttpWsPort        int              `json:"HttpWsPort"`
-	HttpJsonPort      int              `json:"HttpJsonPort"`
-	HttpLocalPort     int              `json:"HttpLocalPort"`
-	NodePort          uint16           `json:"NodePort"`
-	NodeConsensusPort uint16           `json:"NodeConsensusPort"`
-	DualPortSurpport  bool             `json:"DualPortSurpport"`
-	NodeType          string           `json:"NodeType"`
-	PrintLevel        int              `json:"PrintLevel"`
-	IsTLS             bool             `json:"IsTLS"`
-	CertPath          string           `json:"CertPath"`
-	KeyPath           string           `json:"KeyPath"`
-	CAPath            string           `json:"CAPath"`
-	GenBlockTime      uint             `json:"GenBlockTime"`
-	MultiCoreNum      uint             `json:"MultiCoreNum"`
-	MaxLogSize        int64            `json:"MaxLogSize"`
-	MaxTxInBlock      int              `json:"MaxTransactionInBlock"`
-	MaxHdrSyncReqs    int              `json:"MaxConcurrentSyncHeaderReqs"`
-	ConsensusType     string           `json:"ConsensusType"`
-    ConsensusConfigPath string         `json:"ConsensusConfigPath"`
-	SystemFee         map[string]int64 `json:"SystemFee"`
+	Magic               int64            `json:"Magic"`
+	Version             int              `json:"Version"`
+	SeedList            []string         `json:"SeedList"`
+	Bookkeepers         []string         `json:"Bookkeepers"` // The default book keepers' publickey
+	DHTSeeds            []DHTNode        `json:"DHTSeeds"`
+	HttpRestPort        int              `json:"HttpRestPort"`
+	HttpCertPath        string           `json:"HttpCertPath"`
+	HttpKeyPath         string           `json:"HttpKeyPath"`
+	HttpInfoPort        uint16           `json:"HttpInfoPort"`
+	HttpWsPort          int              `json:"HttpWsPort"`
+	HttpJsonPort        int              `json:"HttpJsonPort"`
+	HttpLocalPort       int              `json:"HttpLocalPort"`
+	NodePort            uint16           `json:"NodePort"`
+	NodeConsensusPort   uint16           `json:"NodeConsensusPort"`
+	DualPortSurpport    bool             `json:"DualPortSurpport"`
+	DHTUDPPort          uint16           `json:"DHTUDPPort"`
+	NodeType            string           `json:"NodeType"`
+	PrintLevel          int              `json:"PrintLevel"`
+	IsTLS               bool             `json:"IsTLS"`
+	CertPath            string           `json:"CertPath"`
+	KeyPath             string           `json:"KeyPath"`
+	CAPath              string           `json:"CAPath"`
+	GenBlockTime        uint             `json:"GenBlockTime"`
+	MultiCoreNum        uint             `json:"MultiCoreNum"`
+	MaxLogSize          int64            `json:"MaxLogSize"`
+	MaxTxInBlock        int              `json:"MaxTransactionInBlock"`
+	MaxHdrSyncReqs      int              `json:"MaxConcurrentSyncHeaderReqs"`
+	ConsensusType       string           `json:"ConsensusType"`
+	ConsensusConfigPath string           `json:"ConsensusConfigPath"`
+	SystemFee           map[string]int64 `json:"SystemFee"`
 }
 
 type configFile struct {

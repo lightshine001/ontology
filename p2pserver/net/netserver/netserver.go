d6b3232a3fe46e6a413802a0961f92fe93d4c80b
liupan@onchain.com <liupan@onchain.com>
2018-05-30 17:54:01 +0800

    Add dht configure module under GenesisConfig
    Signed-off-by: liupan@onchain.com <liupan@onchain.com>

--- p2pserver/net/netserver/netserver.go
+++ p2pserver/net/netserver/netserver.go
@@ -23,6 +23,7 @@ import (
 	"fmt"
 	"math/rand"
 	"net"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -62,6 +63,7 @@ type NetServer struct {
 	ConsChan     chan *common.MsgPayload
 	feedCh       chan *types.FeedEvent
 	stopLoop     chan struct{}
+	ConnectingNodes
 	PeerAddrMap
 	Np            *peer.NbrPeers
 	connectLock   sync.Mutex
@@ -158,22 +160,22 @@ func (this *NetServer) loop() {
 	}
 }
 
-func (this *NetServer) handlFeed(event interface{}) {
+func (this *NetServer) handleFeed(event *types.FeedEvent) {
 	switch event.EvtType {
 	case types.Add:
-		node := event.Content.(*types.Node)
+		node := event.Event.(*types.Node)
 		log.Infof("handle feed: add a new node %v", node)
 		address := node.IP + ":" + strconv.Itoa(int(node.TCPPort))
 		this.Connect(address, false)
 	case types.Del:
-		id := event.Content.(types.NodeID)
+		id := event.Event.(types.NodeID)
 		log.Infof("handle feed: remove a node %s", id.String())
 	default:
 		log.Infof("handle feed: unknown feed event %d", event.EvtType)
 	}
 }
 
-func (this *NetServer) SetFeedCh(ch <-chan *types.FeedEvent) {
+func (this *NetServer) SetFeedCh(ch chan *types.FeedEvent) {
 	this.feedCh = ch
 }
 

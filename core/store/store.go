7a53d14db0b50e69b97355a2bcad596584517d21
laizy <laizhichao@onchain.com>
2018-11-20 14:25:47 +0800

    add interface

--- core/store/store.go
+++ core/store/store.go
@@ -23,17 +23,27 @@ import (
 	"github.com/ontio/ontology/common"
 	"github.com/ontio/ontology/core/payload"
 	"github.com/ontio/ontology/core/states"
+	"github.com/ontio/ontology/core/store/overlaydb"
 	"github.com/ontio/ontology/core/types"
 	"github.com/ontio/ontology/smartcontract/event"
 	cstates "github.com/ontio/ontology/smartcontract/states"
 )
+
+type ExecuteResult struct {
+	writeSet   *overlaydb.MemDB
+	Hash       common.Uint256
+	MerkleRoot common.Uint256
+	Notify     []*event.ExecuteNotify
+}
 
 // LedgerStore provides func with store package.
 type LedgerStore interface {
 	InitLedgerStoreWithGenesisBlock(genesisblock *types.Block, defaultBookkeeper []keypair.PublicKey) error
 	Close() error
 	AddHeaders(headers []*types.Header) error
-	AddBlock(block *types.Block) error
+	AddBlock(block *types.Block, merkleRoot common.Uint256) error
+	ExecuteBlock(b *types.Block) (ExecuteResult, error)   // 共识调用
+	SubmitBlock(b *types.Block, exec ExecuteResult) error // 共识调用
 	GetCurrentBlockHash() common.Uint256
 	GetCurrentBlockHeight() uint32
 	GetCurrentHeaderHeight() uint32
